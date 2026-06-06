9b37434c80a07242acdbef17f36f21b77d28fb92
xebec19 <rohandeveloper106@gmail.com>
2026-06-06 18:19:38 +0530

    wip worker

--- queues/worker.py
+++ queues/worker.py
@@ -21,40 +21,45 @@ vectordb = QdrantVectorStore.from_existing_collection(
 
 def process_query(query: str):
 
-    search_results = vectordb.similarity_search(query=query, k=5)
+    try:
+        search_results = vectordb.similarity_search(query=query, k=5)
 
-    if not search_results:
-        print("No releant documents found for your query")
-        return
+        if not search_results:
+            print("No releant documents found for your query")
+            return
 
-    context = "\n\n\n".join(
-        [
-            f"Page Content: {r.page_content}\n"
-            f"Page Number: {r.metadata.get('page_label', 'N/A')}\n"
-            f"File: {r.metadata.get('source', 'Unknown')}"
-            for r in search_results
-        ]
-    )
+        context = "\n\n\n".join(
+            [
+                f"Page Content: {r.page_content}\n"
+                f"Page Number: {r.metadata.get('page_label', 'N/A')}\n"
+                f"File: {r.metadata.get('source', 'Unknown')}"
+                for r in search_results
+            ]
+        )
 
-    SYSTEM_PROMPT = """
-    You are a helpful AI Assistant who answers user queries based on the context below.
-    Only answer from the context and guide the user to the right page number.
+        SYSTEM_PROMPT = """
+        You are a helpful AI Assistant who answers user queries based on the context below.
+        Only answer from the context and guide the user to the right page number.
 
-    Context: {context}
-    """
+        Context: {context}
+        """
 
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-3.1-flash-lite", temperature=0.2, max_retries=3
-    )
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", SYSTEM_PROMPT),
-            ("human", "{input}"),
-        ]
-    )
+        llm = ChatGoogleGenerativeAI(
+            model="gemini-3.1-flash-lite", temperature=0.2, max_retries=3
+        )
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", SYSTEM_PROMPT),
+                ("human", "{input}"),
+            ]
+        )
 
-    chain = prompt | llm
-    response = chain.invoke({"input": query, "context": context})
-    print(response.content)
+        chain = prompt | llm
+        response = chain.invoke({"input": query, "context": context})
+        print(response.content)
 
-    return response.content
+        return response.content
+
+    except Exception as err:
+        print(f"Error: {err}")
+        return "request failed"
--- server.py
+++ server.py
@@ -15,3 +15,11 @@ def chat(query: str = Query(..., description="The chat query of user")):
     job = queue.enqueue(process_query, query)
 
     return {"status": "queued", "job": job.id}
+
+
+@app.get("/job-stats")
+def get_result(job_id: str = Query(..., description="Job ID")):
+    job = queue.fetch_job(job_id=job_id)
+    result = job.return_value()
+
+    return {"result": result}
